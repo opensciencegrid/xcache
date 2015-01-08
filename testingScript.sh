df3dd7e78e7fb295202a7559b78b19945052a6f3
Anna Olson <olsona@cs.uchicago.edu>
2015-01-08 12:11:01 -0600

    debugging

--- testingScript.sh
+++ testingScript.sh
@@ -4,6 +4,8 @@ source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
 module load xrootd/4.1.1 2>&1
 source ./setStashCache.sh
 arrFiles=$(echo $1 | tr ",")
+echo $arrFiles
 for f in $arrFiles; do
-	bash ./stashcp -d -s $f -l $2
+    echo $f
+#    bash ./stashcp -d -s $f -l $2
 done 
