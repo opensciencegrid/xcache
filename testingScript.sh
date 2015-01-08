fbf11fc1c35f06d5c03d59f6bb534b563bd35aa6
Anna Olson <olsona@cs.uchicago.edu>
2015-01-08 12:13:41 -0600

    debugging

--- testingScript.sh
+++ testingScript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
-module load xrootd/4.1.1 2>&1
-source ./setStashCache.sh
+#source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
+#module load xrootd/4.1.1 2>&1
+#source ./setStashCache.sh
 arrFiles=$(echo $1 | tr ",")
 echo $arrFiles
 for f in $arrFiles; do
