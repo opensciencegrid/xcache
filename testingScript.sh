55e11d91c6e6fb91efa300bd79d6ace9c069c0f2
olsona <olsona@cs.uchicago.edu>
2015-01-08 11:50:55 -0600

    for multiple files

--- testingScript.sh
+++ testingScript.sh
@@ -3,4 +3,7 @@
 source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
 module load xrootd/4.1.1 2>&1
 source ./setStashCache.sh
-bash ./stashcp -d -s $1 -l $2 
+arrFiles=$(echo $1 | tr ",")
+for f in $arrFiles; do
+	bash ./stashcp -d -s $f -l $2
+done 
