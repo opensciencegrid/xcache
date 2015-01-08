005c8a37db0fa181c086b1352fec9d4bc44e82e6
Anna Olson <olsona@cs.uchicago.edu>
2015-01-08 14:28:41 -0600

    cleaning up

--- simpleScript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-arrFiles=$(echo $1 | tr "," " ")
-echo $arrFiles
-for f in $arrFiles; do
-    echo $f
-done
--- testingScript.sh
+++ testingScript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
-#module load xrootd/4.1.1 2>&1
-#source ./setStashCache.sh
+source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
+module load xrootd/4.1.1 2>&1
+source ./setStashCache.sh
 arrFiles=$(echo $1 | tr ",")
 echo $arrFiles
 for f in $arrFiles; do
     echo $f
-#    bash ./stashcp -d -s $f -l $2
+    bash ./stashcp -d -s $f -l $2
 done 
