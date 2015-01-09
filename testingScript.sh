909d68281642b4b123d68825ccb4a66bb5fee8f7
olsona <olsona@cs.uchicago.edu>
2015-01-09 16:37:50 -0600

    I really hope that the problem isn't here

--- testingScript.sh
+++ testingScript.sh
@@ -2,7 +2,9 @@
 
 source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
 module load xrootd/4.1.1 2>&1
+echo "$(date): Loaded XrootD"
 source ./setStashCache.sh
+echo "$(date): Got prefix"
 arrFiles=$(echo $1 | tr "," "\n") 
 for f in $arrFiles; do
     echo $f
