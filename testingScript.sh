c756e9014da2b626755a232b3470329eee6e0f89
olsona <olsona@cs.uchicago.edu>
2015-01-09 17:50:25 -0600

    I really hope that the problem isn't here

--- testingScript.sh
+++ testingScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
-module load xrootd/4.1.1 2>&1
+module load xrootd/4.1.1 
 echo "$(date): Loaded XrootD"
 source ./setStashCache.sh
 echo "$(date): Got prefix"
