eb5a87dc45da656c8471fe04b643fd525ff69c0a
Anna Olson <olsona@cs.uchicago.edu>
2015-01-20 11:26:29 -0600

    No wget for IU testing

--- testingScript.sh
+++ testingScript.sh
@@ -11,16 +11,16 @@ for f in $arrFiles; do
     bash ./stashcp -d -s $f -l $2 2>&1
     file=$(echo $f | rev | cut -d'/' -f1 | rev)
     rm $2/$file
-    f1=$(echo $f | cut -d'/' -f4-)
-    st=$(date +%s%3N)
-    wget http://stash.osgconnect.net/+olsona/$f1 -P $2 2>&1
-    dl=$(date +%s%3N)
-    if [ -s $2/$file ]; then
-	size=$(stat --printf="%s" $2/$file)
-	dltm=$((dl-st))
-	printf "Source: STASH\tFile: %s\tTime: %s ms\tStart: %s\tSize: %s B\tSpeed: %s KBps\n" "$file" "$dltm" "$st" "$size" "$((size/dltm))"
-	rm $2/$file
-    else
-	printf "WGET of $file failed."
-    fi
+    #f1=$(echo $f | cut -d'/' -f4-)
+    #st=$(date +%s%3N)
+    #wget http://stash.osgconnect.net/+olsona/$f1 -P $2 2>&1
+    #dl=$(date +%s%3N)
+    #if [ -s $2/$file ]; then
+    #size=$(stat --printf="%s" $2/$file)
+#	dltm=$((dl-st))
+#	printf "Source: STASH\tFile: %s\tTime: %s ms\tStart: %s\tSize: %s B\tSpeed: %s KBps\n" "$file" "$dltm" "$st" "$size" "$((size/dltm))"
+#	rm $2/$file
+#    else
+#	printf "WGET of $file failed."
+#    fi
 done 
