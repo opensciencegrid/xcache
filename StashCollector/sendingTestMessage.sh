43ef69e72001cfde11133db9f4f95ca1a2279aa8
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2015-02-16 11:03:03 -0600

    Update sendingTestMessage.sh

--- StashCollector/sendingTestMessage.sh
+++ StashCollector/sendingTestMessage.sh
@@ -14,9 +14,9 @@ sc="stashCache1.org"
 
 hn=$(hostname)
 timestamp=$(date +%s)
-header=[{ "headers" : {"timestamp" : "'${timestamp}'", "host" : "'${hn}'" },'
-body='"body" : "'${timestamp}','${fname}','${fsize}','${tcopy}','${cs}','${sc}'" }]
+header="[{ \"headers\" : {\"timestamp\" : \"${timestamp}\", \"host\" : \"${hn}\" },"
+body="\"body\" : \"${timestamp},${fname},${fsize},${tcopy},${cs},${sc}\" }]"
 echo $header$body > data.json
 
-curl -X POST -H 'Content-Type: application/json; charset=UTF-8' -d $data.json http://hadoop-dev.mwt2.org:80/
+curl -X POST -H 'Content-Type: application/json; charset=UTF-8' http://hadoop-dev.mwt2.org:80/ -d @data.json
 rm data.json
