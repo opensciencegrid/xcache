de645949467a9473b6c8297ce11cc9656f92a508
olsona <olsona@cs.uchicago.edu>
2015-04-25 15:44:40 -0500

    refactor debugging

--- bin/stashcp_refactor.sh
+++ bin/stashcp_refactor.sh
@@ -109,30 +109,26 @@ function doStashCpDirectory {
 	lc=$(echo "${source: -1}")
 	if [ $lc != "/" ] || [ recursive == 1 ]; then
 		dirname=$(echo $source | rev | cut -d/ -f1 | rev)
-		loc=$loc/$dirname
-		mkdir -p $loc
+		myLocalLoc=$loc/$dirname
+		#loc=$loc/$dirname
+		mkdir -p $myLocalLoc
 		sourceName="$source/+"
 	else
+		myLocalLoc=$loc
 		sourceName="$source+"
 	fi
 	sz=$(xrdfs root://data.ci-connect.net stat $source | grep "Size: " | cut -d':' -f2)
 	sz=$(echo -n "${sz//[[:space:]]/}")
 	st=$(date +%s%3N)
-	dirs=()
 	for sfile in $sfiles; do
 		echo $sfile
 		isdir=$(xrdfs root://data.ci-connect.net stat $sfile | grep "IsDir" | wc -l)
 		if [ $isdir != 0 ] && [ $recursive == 1 ]; then
 			echo "$sfile is directory; will copy"
-			doStashCpDirectory $sfile $loc
-			dirs=("${dirs[@]}" $sfile)
+			doStashCpDirectory $sfile $myLocalLoc
 		elif [ $isdir == 0 ]; then
-			doStashCpSingle $sfile $loc
+			doStashCpSingle $sfile $myLocalLoc
 		fi
-	done
-	echo "Dirs:"
-	for dir in $dirs; do
-		echo $dir;
 	done
 	dl=$(date +%s%3N)
 	dltm=$((dl-st))
