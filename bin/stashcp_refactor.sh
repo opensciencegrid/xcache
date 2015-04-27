c34c3a3dd61d9b813340aeaaab90386e560e3bdf
olsona <olsona@cs.uchicago.edu>
2015-04-27 15:45:16 -0500

    debugging refactor

--- bin/stashcp_refactor.sh
+++ bin/stashcp_refactor.sh
@@ -121,7 +121,7 @@ function doStashCpDirectory {
 		isdir=$(xrdfs root://data.ci-connect.net stat $sfile | grep "IsDir" | wc -l)
 		if [ $isdir != 0 ] && [ $recursive == 1 ]; then
 			echo "$sfile is directory; will copy"
-			relPath=$(echo $sfile | sed 's/^$prefix//')
+			relPath=$(echo $sfile | sed 's/^\/$prefix//')
 			echo "My relative path is: $relPath"
 			mkdir -p $myLoc/$relPath
 			doStashCpDirectory $sfile $myLoc/$relPath
@@ -285,9 +285,8 @@ for file in ${files[@]}; do
 			doStashCpDirectory $file $loc update
 		else
 			dir=$(echo $source | rev | cut -d/ -f1 | rev)
-			ind=$(strIndex $source $dir)
-			echo "Index is: $ind"
-			echo "Prefix?: ${source:0:ind}"
+			export prefix=$(echo $source | rev | cut -d/ -f2- | rev)
+			echo "My source prefix is $prefix"
 			mkdir $loc/$dir
 			doStashCpDirectory $file $loc/$dir update
 		fi
