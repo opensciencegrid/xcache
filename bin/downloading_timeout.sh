c5976e8eaa54fb7c88e4dc8fda50ddee1a40fbb3
Tony Aburaad <tony.aburaad@gmail.com>
2016-07-07 10:38:31 -0500

    Fix download monitor (#6)
    Update download_timeout.sh so that it dies kills the transfer and itself correctly.
    
    Update the ES index and data.

--- bin/downloading_timeout.sh
+++ bin/downloading_timeout.sh
@@ -1,57 +1,51 @@
-#!/bin/bash
 while getopts "t:f:d:s:" opt; do
   case "$opt" in
       t) timeout=$OPTARG ;;
-	  f) file=$OPTARG ;;
-	  d) diff=$OPTARG ;;
-	  s) expSize=$OPTARG ;;
+      f) file=$OPTARG ;;
+      d) diff=$OPTARG ;;
+      s) expSize=$OPTARG ;;
   esac
 done
 shift $((OPTIND-1))
 
+
 start_watchdog(){
-	timeout="$1"
-	file="$2"
-	diff="$3"
-	expSize="$4"
-	(( i = timeout ))
-	prevSize=0
-	while (( i > 0 ))
-	do
-		if [ -e $file ]; then
-			newSize=$(du -b $file | cut -f1)
-			nextSize=$((prevSize+diff))
-			wantSize=$((nextSize<expSize?nextSize:expSize))
-			if [ $newSize -lt $((wantSize)) ]; then
-				# kill -0 $$ || exit 0
-				sleep 1
-				(( i -= 1 ))
-			elif [ $newSize -eq $expSize ]; then
-				# Finished file
-				exit 0
-			else
-				prevSize=$(du -b $file | cut -f1)
-				(( i = timeout ))
-				sleep 1
-			fi
-		else
-			sleep 1
-			(( i -= 1 )) ## to avoid infinite loop
-		fi
-	done
-	
-	echo "killing process after timeout of $timeout seconds"
-	kill $$
+    timeout=$1
+    file=$2
+    diff=$3
+    expSize=$4
+    xrdpid=$5
+    prevSize=0
+    while (( newSize<expSize ))
+    do 
+        sleep $timeout #check status after every x seconds
+        if [ -e $file ]; then
+            newSize=$(du -b $file | cut -f1)
+            nextSize=$((prevSize+diff))
+            wantSize=$((nextSize<expSize?nextSize:expSize))
+            if [ $newSize -lt $wantSize ]; then #if time out
+                kill -9 $xrdpid
+            else #if file increases accordingly
+                prevSize=$(du -b $file | cut -f1)
+            fi
+        else
+            kill -9 $xrdpid
+        fi
+    done   
 }
 
+#Execute xrdcp
+"$@" &
+xrdpid=$!
 
-start_watchdog "$timeout" "$file" "$diff" "$expSize" &
+start_watchdog "$timeout" "$file" "$diff" "$expSize" "$xrdpid" &
 watchdog_pid=$!
-"$@"
-cp_exit=$?
 
-# If the cp command exits, kill the watchdog
-kill $watchdog_pid
-exit $cp_exit
+wait $xrdpid
+xrd_exit=$?
+
+# If xrdcp command exits, kill the watchdog and exit using xrdcp's exit code
+kill $watchdog_pid 2>/dev/null
+exit $xrd_exit
 
 ## Based on: http://fahdshariff.blogspot.com/2013/08/executing-shell-command-with-timeout.html
