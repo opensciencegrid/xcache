5e3ab9381f872f709d59260815293406877f2512
Tony Aburaad <tony.aburaad@gmail.com>
2016-10-11 09:34:38 -0500

    need to include tests here

--- bin/stashcp2/tests/test.sh
+++
@@ -1,4 +0,0 @@
-python stashcp.py --debug user/taburaad/public/2gb_file.tar .
-
-ls -l 2gb_file.tar  
-rm -f 2gb_file.tar 
---
+++ bin/stashcp2/tests/test_inside_docker.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -xe
+
+OS_VERSION=$1
+
+ls -l /home
+
+# Clean the yum cache
+yum -y clean all
+yum -y clean expire-cache
+
+# First, install all the needed packages.
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${OS_VERSION}.noarch.rpm
+
+# Broken mirror?
+echo "exclude=mirror.beyondhosting.net" >> /etc/yum/pluginconf.d/fastestmirror.conf
+
+yum -y install yum-plugin-priorities
+rpm -Uvh https://repo.grid.iu.edu/osg/3.3/osg-3.3-el${OS_VERSION}-release-latest.rpm
+yum -y install rpm-build gcc gcc-c++ boost-devel cmake git tar gzip make autotools
+
+# Prepare the RPM environment
+mkdir -p /tmp/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+cat >> /etc/rpm/macros.dist << EOF
+%dist .osg.el${OS_VERSION}
+%osg 1
+EOF
+
+# Perform tests
