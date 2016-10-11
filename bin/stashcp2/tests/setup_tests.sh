7e11afabe96fda914b5bf23a3fac6788d9c5e881
Tony Aburaad <tony.aburaad@gmail.com>
2016-10-11 09:27:35 -0500

    travis and setup_tests

--- bin/stashcp2/tests/setup_tests.sh
+++ bin/stashcp2/tests/setup_tests.sh
@@ -8,14 +8,14 @@ el_version=$1
  # Run tests in Container
 if [ "$el_version" = "6" ]; then
 
-sudo docker run --rm=true -v `pwd`:/stashcp:rw centos:centos${OS_VERSION} /bin/bash -c "bash -xe /stashcp/tests/test_inside_docker.sh ${OS_VERSION}"
+sudo docker run --rm=true -v `pwd`:/StashCache:rw centos:centos${OS_VERSION} /bin/bash -c "bash -xe /StashCache/bin/stashcp2/tests/test_inside_docker.sh ${OS_VERSION}"
 
 elif [ "$el_version" = "7" ]; then
 
-docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/stashcp:rw  centos:centos${OS_VERSION}   /usr/sbin/init
+docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/StashCache:rw  centos:centos${OS_VERSION}   /usr/sbin/init
 DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
 docker logs $DOCKER_CONTAINER_ID
-docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /stashcp/tests/test_inside_docker.sh ${OS_VERSION};
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /StashCache/bin/stashcp2/tests/test_inside_docker.sh ${OS_VERSION};
 echo -ne \"------\nEND stashcp TESTS\n\";"
 docker ps -a
 docker stop $DOCKER_CONTAINER_ID
