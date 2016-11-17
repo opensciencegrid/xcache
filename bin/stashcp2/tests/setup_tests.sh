5cfb6f70135549e8a4fbd7d7ddc6786e96bc06ac
Derek Weitzel <djw8605@gmail.com>
2016-11-17 14:43:16 -0600

    More privledged

--- bin/stashcp2/tests/setup_tests.sh
+++ bin/stashcp2/tests/setup_tests.sh
@@ -8,7 +8,7 @@ el_version=$1
  # Run tests in Container
 if [ "$el_version" = "6" ]; then
 
-sudo docker run --cap-add SYS_ADMIN --rm=true -v `pwd`:/StashCache:rw centos:centos${OS_VERSION} /bin/bash -c "bash -xe /StashCache/bin/stashcp2/tests/test_inside_docker.sh ${OS_VERSION}"
+sudo docker run --privileged --rm=true -v `pwd`:/StashCache:rw centos:centos${OS_VERSION} /bin/bash -c "bash -xe /StashCache/bin/stashcp2/tests/test_inside_docker.sh ${OS_VERSION}"
 
 elif [ "$el_version" = "7" ]; then
 
