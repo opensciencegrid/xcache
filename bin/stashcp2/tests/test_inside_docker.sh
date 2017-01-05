2e18708a7dae8ec3318ca4284a3afe851e40815b
Derek Weitzel <djw8605@gmail.com>
2017-01-05 14:12:08 -0600

    Use destination filename for checking transferred file size
    Fixes #39

--- bin/stashcp2/tests/test_inside_docker.sh
+++ bin/stashcp2/tests/test_inside_docker.sh
@@ -37,6 +37,17 @@ module load xrootd
 # For now, disable pylint failures
 pylint /StashCache/bin/stashcp || /bin/true
 
+# Try copying with different destintion filename
+/StashCache/bin/stashcp --cache=$XRD_CACHE -d /user/dweitzel/public/blast/queries/query1 query.test
+
+result=`md5sum query.test | awk '{print $1;}'`
+
+if [ "$result" != "12bdb9a96cd5e8ca469b727a81593201" ]; then
+  exit 1
+fi
+
+rm -f query.test
+
 # Perform tests
 /StashCache/bin/stashcp --cache=$XRD_CACHE -d /user/dweitzel/public/blast/queries/query1 ./
 
